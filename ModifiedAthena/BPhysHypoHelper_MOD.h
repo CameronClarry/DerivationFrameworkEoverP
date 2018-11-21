fe391a6928b8a8637974f14be482a94e96bcbd05
Lukas Adamek <lukas.adamek@cern.ch>
2018-11-21 21:08:51 +0100

    Replace BPhysHypohelper files with those from 21.2 branch

--- ModifiedAthena/BPhysHypoHelper_MOD.h
+++ ModifiedAthena/BPhysHypoHelper_MOD.h
@@ -1,7 +1,5 @@
 // Dear emacs, this is -*- c++ -*
 
-// f7e8cfd3048b19a98ba2121a588c6da6cd207e95
-
 /*
   Copyright (C) 2002-2017 CERN for the benefit of the ATLAS collaboration
 */
@@ -80,153 +78,155 @@ namespace xAOD {
 
   class BPhysHypoHelper : public BPhysHelper {
 
-  public:
+    public:
       
-    /************************************************************************/
-    /** @brief: Main constructor 
-     *
-     *  The Main constructor.
-     *  BPhysHypoHelper does NOT take ownership of the class "b"
-     *
-     *  @param[in] hypo  Name of the hypothesis
-     *  @param[in] b     Pointer to the xAOD::Vertex
-     */
-  BPhysHypoHelper(const std::string hypo, const xAOD::Vertex* b) :
-    BPhysHelper(b),
-      m_hypo(hypo)
+      /************************************************************************/
+      /** @brief: Main constructor 
+       *
+       *  The Main constructor.
+       *  BPhysHypoHelper does NOT take ownership of the class "b"
+       *
+       *  @param[in] hypo  Name of the hypothesis
+       *  @param[in] b     Pointer to the xAOD::Vertex
+       */
+      BPhysHypoHelper(const std::string hypo, const xAOD::Vertex* b) :
+        BPhysHelper(b),
+        m_hypo(hypo)
       {
         /* nothing here */
       }
       
-    /************************************************************************/
-    /** @{
-     *  Methods for access to the invariant mass hypothesis and its error
-     */
+      /************************************************************************/
+      /** @{
+       *  Methods for access to the invariant mass hypothesis and its error
+       */
       
-    /** Get invariant mass and its error
-     * 
-     *  @returns: mass or error, -9999999. in case the augmentation doesn't exist
-     */
+      /** Get invariant mass and its error
+       * 
+       *  @returns: mass or error, -9999999. in case the augmentation doesn't exist
+       */
       
-    float mass();     //!< invariant mass
-    float massErr();  //!< invariant mass error
+      float mass();     //!< invariant mass
+      float massErr();  //!< invariant mass error
 
-    /** Set given invariant mass and its error
-     * 
-     *  @param[in] val   invariant mass or error calculated externally
-     *  @returns: true on success
-     */
+      /** Set given invariant mass and its error
+       * 
+       *  @param[in] val   invariant mass or error calculated externally
+       *  @returns: true on success
+       */
       
-    bool setMass   (const float val);  //!< invariant mass
-    bool setMassErr(const float val);  //!< invariant mass error
+      bool setMass   (const float val);  //!< invariant mass
+      bool setMassErr(const float val);  //!< invariant mass error
       
-    /** Set invariant mass for given track mass hypotheses. The number of mass hyotheses
-     *  must be equal to the number of refitted tracks!
-     * 
-     * 
-     *  @param[in] trkMasses   masses of the tracks.    
-     *  @returns: true on success
-     */
+      /** Set invariant mass for given track mass hypotheses. The number of mass hyotheses
+       *  must be equal to the number of refitted tracks!
+       * 
+       *  @param[in] trkMasses   masses of the tracks.    
+       *  @returns: true on success
+       */
       
-    bool setMass(const std::vector<double> &trkMasses);
+      bool setMass(const std::vector<double> &trkMasses);
       
-    /** @} */
-    /************************************************************************/
+      /** @} */
+      /************************************************************************/
 
-    /** @{
-     *  Methods for access to the proper decay time and its error
-     */
+      /** @{
+       *  Methods for access to the proper decay time and its error
+       */
       
-    /** @brief: Enum type the proper decay time calculation
-     * 
-     *  @value: TAU_CONST_MASS  the proper decay time calculated with the constant mass hypothesis
-     *  @value: TAU_INV_MASS    the proper decay time calculated with the measured invariant mass
-     */
+      /** @brief: Enum type the proper decay time calculation
+       * 
+       *  @value: TAU_CONST_MASS  the proper decay time calculated with the constant mass hypothesis
+       *  @value: TAU_INV_MASS    the proper decay time calculated with the measured invariant mass
+       */
       
-    enum tau_type { TAU_CONST_MASS, TAU_INV_MASS };
+      enum tau_type { TAU_CONST_MASS, TAU_INV_MASS };
                     
-    /** @brief: Get the proper decay time and error. 
-     * 
-     *  The proper decay time is calculated using formula c*tau = Lxy * M / pT. The Lxy can be calculated
-     *  wrt different choice of the PV. Use parameter vertexType to specify which PV was used.
-     *  The particle's mass M can be either taken as a constant or as measured invariant mass. Use
-     *  parameter tauType to specify which value should be used.
-     * 
-     *  @param[in] vertexType  type (selection) of the PV (@see: BPhysHelper::pv_type)
-     *  @param[in] tauType     specify which value of particle's mass was used (@see: tau_type)
-     *  @returns: proper decay time, -9999999. on error       
-     */
+      /** @brief: Get the proper decay time and error. 
+       * 
+       *  The proper decay time is calculated using formula c*tau = Lxy * M / pT. The Lxy can be calculated
+       *  wrt different choice of the PV. Use parameter vertexType to specify which PV was used.
+       *  The particle's mass M can be either taken as a constant or as measured invariant mass. Use
+       *  parameter tauType to specify which value should be used.
+       * 
+       *  @param[in] vertexType  type (selection) of the PV (@see: BPhysHelper::pv_type)
+       *  @param[in] tauType     specify which value of particle's mass was used (@see: tau_type)
+       *  @returns: proper decay time, -9999999. on error       
+       */
       
-    float tau(const pv_type vertexType = BPhysHelper::PV_MIN_A0,
-	      const tau_type tauType = BPhysHypoHelper::TAU_CONST_MASS);  //!< proper decay time
-                    
-    float tauErr(const pv_type vertexType = BPhysHelper::PV_MIN_A0,
-		 const tau_type tauType = BPhysHypoHelper::TAU_CONST_MASS);  //!< proper decay time error
-
-    /** @brief: Set the proper decay time and error. 
-     *  @param[in] val         input value
-     *  @param[in] vertexType  type (selection) of the PV (@see: BPhysHelper::pv_type)
-     *  @param[in] tauType     specify which value of particle's mass was used (@see: tau_type)
-     *  @returns: true on success
-     */
-      
-    bool setTau(const float val,
-		const pv_type vertexType = BPhysHelper::PV_MIN_A0,
-		const tau_type tauType = BPhysHypoHelper::TAU_CONST_MASS);  //!< proper decay time
-                    
-    bool setTauErr(const float val, 
-		   const pv_type vertexType = BPhysHelper::PV_MIN_A0,
-		   const tau_type tauType = BPhysHypoHelper::TAU_CONST_MASS);  //!< proper decay time error
-    //3D values
-    float tau3d(const pv_type vertexType = BPhysHelper::PV_MIN_A0,
+      float tau(const pv_type vertexType = BPhysHelper::PV_MIN_A0,
                 const tau_type tauType = BPhysHypoHelper::TAU_CONST_MASS);  //!< proper decay time
                     
-    float tau3dErr(const pv_type vertexType = BPhysHelper::PV_MIN_A0,
+      float tauErr(const pv_type vertexType = BPhysHelper::PV_MIN_A0,
                    const tau_type tauType = BPhysHypoHelper::TAU_CONST_MASS);  //!< proper decay time error
 
-    bool setTau3d(const float val,
+      /** @brief: Set the proper decay time and error. 
+       *  @param[in] val         input value
+       *  @param[in] vertexType  type (selection) of the PV (@see: BPhysHelper::pv_type)
+       *  @param[in] tauType     specify which value of particle's mass was used (@see: tau_type)
+       *  @returns: true on success
+       */
+      
+      bool setTau(const float val,
                   const pv_type vertexType = BPhysHelper::PV_MIN_A0,
                   const tau_type tauType = BPhysHypoHelper::TAU_CONST_MASS);  //!< proper decay time
                     
-    bool setTau3dErr(const float val, 
+      bool setTauErr(const float val, 
+                     const pv_type vertexType = BPhysHelper::PV_MIN_A0,
+                     const tau_type tauType = BPhysHypoHelper::TAU_CONST_MASS);  //!< proper decay time error
+//3D values
+      float tau3d(const pv_type vertexType = BPhysHelper::PV_MIN_A0,
+                const tau_type tauType = BPhysHypoHelper::TAU_CONST_MASS);  //!< proper decay time
+                    
+      float tau3dErr(const pv_type vertexType = BPhysHelper::PV_MIN_A0,
+                   const tau_type tauType = BPhysHypoHelper::TAU_CONST_MASS);  //!< proper decay time error
+
+      bool setTau3d(const float val,
+                  const pv_type vertexType = BPhysHelper::PV_MIN_A0,
+                  const tau_type tauType = BPhysHypoHelper::TAU_CONST_MASS);  //!< proper decay time
+                    
+      bool setTau3dErr(const float val, 
                      const pv_type vertexType = BPhysHelper::PV_MIN_A0,
                      const tau_type tauType = BPhysHypoHelper::TAU_CONST_MASS);  //!< proper decay time error
 
-    /** @} */
-    /************************************************************************/
+      /** @} */
+      /************************************************************************/
 
-    /** @{
-     *  Methods for access to the pass flags
-     */
+      /** @{
+       *  Methods for access to the pass flags
+       */
       
-    /** get the pass flag for this hypothesis
-     *  @return: true, if the candidate passed selection for this hypothesis
-     */
-    bool pass() const;
+      /** get the pass flag for this hypothesis
+       *  @return: true, if the candidate passed selection for this hypothesis
+       */
+      bool pass() const;
       
-    /** get the pass flag for this hypothesis
-     *  @param[in] input value. Will be converted to Char_t
-     *  @return: true on success
-     */
-    bool setPass(bool passVal);
+      /** get the pass flag for this hypothesis
+       *  @param[in] input value. Will be converted to Char_t
+       *  @return: true on success
+       */
+      bool setPass(bool passVal);
       
 
 
-    /** @} */
-    /************************************************************************/
+      /** @} */
+      /************************************************************************/
       
-  protected:
+    protected:
       
-    /** @{
-     * Private attributes:
-     */
+      /** @{
+       * Private attributes:
+       */
       
-    std::string m_hypo;   //!< name of the mass hypothesis
-     
-    /** @} */
+      std::string m_hypo;   //!< name of the mass hypothesis
+      
+      /** @} */
       
       
   };
 }
 
 #endif
+  
+
+
