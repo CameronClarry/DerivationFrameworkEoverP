5b21e1fdd37fa01e1e5f233aa029137c305316d1
Cameron Clarry <cameron.clarry@cern.ch>
2022-10-04 21:45:17 +0200

    Replace DECLARE_TOOL_FACTORY with DECLARE_COMPONENT

--- src/components/DerivationFrameworkEoverP_entries.cxx
+++ src/components/DerivationFrameworkEoverP_entries.cxx
@@ -1,17 +1,9 @@
-#include "GaudiKernel/DeclareFactoryEntries.h"
 #include "DerivationFrameworkEoverP/TrackCaloDecorator.h"
 #include "DerivationFrameworkEoverP/Reco_mumu.h"
 #include "DerivationFrameworkEoverP/Select_onia2mumu.h"
 
 using namespace DerivationFramework;
 
-DECLARE_TOOL_FACTORY( TrackCaloDecorator )
-DECLARE_TOOL_FACTORY( Reco_mumu )
-DECLARE_TOOL_FACTORY( Select_onia2mumu )
-
-DECLARE_FACTORY_ENTRIES( DerivationFrameworkEoverP ) 
-{
-  DECLARE_TOOL( TrackCaloDecorator );
-  DECLARE_TOOL( Reco_mumu );
-  DECLARE_TOOL( Select_onia2mumu );
-}
+DECLARE_COMPONENT( TrackCaloDecorator )
+DECLARE_COMPONENT( Reco_mumu )
+DECLARE_COMPONENT( Select_onia2mumu )
